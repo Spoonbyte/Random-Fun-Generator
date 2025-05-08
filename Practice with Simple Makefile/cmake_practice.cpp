0dd09c37d2cb3be316e0d558ecf979bd52cc7ee5
Spoonbyte <sanjay.poonaisingh@gmail.com>
2025-05-08 16:45:16 -0400

    Initial commit

---
+++ Practice with Simple Makefile/cmake_practice.cpp
@@ -0,0 +1,18 @@
+/**
+ * cmake_practice.cpp
+ * [Description of the program]
+ * 2025-05-06
+ *
+ * Created by spoonais on 2025-05-06.
+ */
+
+#include <iostream>
+#include "function.h"
+using namespace std;
+
+int main() {
+    
+    hellow();    
+    return 0;
+}
+
---
+++ Practice with Simple Makefile/function.cpp
@@ -0,0 +1,15 @@
+/**
+ * function.cpp
+ * [Description of the program]
+ * 2025-05-08
+ *
+ * Created by spoonais on 2025-05-08.
+ */
+
+#include <iostream>
+using namespace std;
+
+void hellow()
+{
+    cout << "Hello World" << endl;
+}
